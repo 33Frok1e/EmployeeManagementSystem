a8ba247b697b3a20ff76fa8180b1df1661c87d6e
33Frok1e <mratikrishna@gmail.com>
2025-01-01 16:07:49 +0530

    Role Added

---
+++ ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagementSystem.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [DisplayName("Role")]
+        public string RoleName { get; set; }
+    }
+}
