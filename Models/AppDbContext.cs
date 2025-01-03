daa89f347e4145c015b22c8ad49135d5540cfa78
33Frok1e <mratikrishna@gmail.com>
2025-01-03 16:49:56 +0530

    Enforce ON DELETE NO ACTION

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -15,6 +15,11 @@ namespace EmployeeManagementSystem.Models
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Seed();
+
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
         }
     }
 }
